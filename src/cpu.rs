089500ec6742fbc2bb76345989a42e92600161e2
Shun2439 <shunta.kobayashi24@gmail.com>
2025-07-10 17:43:15 +0900

    style: split to new function

--- src/cpu.rs
+++ src/cpu.rs
@@ -27,6 +27,67 @@ pub(crate) static mut REG: [i16; 8] = [0; 8];
 pub(crate) static mut ROM: [i16; 256] = [0; 256];
 pub(crate) static mut RAM: [i16; 256] = [0; 256];
 
+pub fn cpu() {
+    let mut pc;
+    let mut ir: i16;
+    let mut flag_eq: bool;
+
+    assembler();
+
+    pc = 0;
+    flag_eq = false;
+
+    loop {
+        unsafe {
+            ir = ROM[pc];
+            println!(
+                "{:?} {:?} {:?} {:?} {:?} {:?}",
+                pc, ir, REG[0], REG[1], REG[2], REG[3]
+            );
+        }
+
+        pc += 1;
+
+        // core
+        unsafe {
+            match op_code(ir) {
+                MOV => REG[op_reg_a(ir) as usize] = REG[op_reg_b(ir) as usize],
+                ADD => REG[op_reg_a(ir) as usize] += REG[op_reg_b(ir) as usize],
+                SUB => REG[op_reg_a(ir) as usize] -= REG[op_reg_b(ir) as usize],
+                AND => REG[op_reg_a(ir) as usize] &= REG[op_reg_b(ir) as usize],
+                OR => REG[op_reg_a(ir) as usize] |= REG[op_reg_b(ir) as usize],
+                SL => REG[op_reg_a(ir) as usize] = REG[op_reg_a(ir) as usize] << 1,
+                SR => REG[op_reg_a(ir) as usize] = REG[op_reg_a(ir) as usize] >> 1,
+                SRA => {
+                    REG[op_reg_a(ir) as usize] = (REG[op_reg_a(ir) as usize] & 0x8000u16 as i16)
+                        | (REG[op_reg_a(ir) as usize] >> 1)
+                }
+                LDL => {
+                    REG[op_reg_a(ir) as usize] =
+                        (REG[op_reg_a(ir) as usize] & 0xff00u16 as i16) | (op_data(ir) & 0x00ff)
+                }
+                LDH => {
+                    REG[op_reg_a(ir) as usize] =
+                        (op_data(ir) << 8) | (REG[op_reg_a(ir) as usize] & 0x00ff);
+                }
+                CMP => flag_eq = REG[op_reg_a(ir) as usize] == REG[op_reg_b(ir) as usize],
+                JE => {
+                    if flag_eq {
+                        pc = op_addr(ir) as usize;
+                    }
+                }
+                JMP => pc = op_addr(ir) as usize,
+                LD => REG[op_reg_a(ir) as usize] = RAM[op_addr(ir) as usize],
+                ST => RAM[op_addr(ir) as usize] = REG[op_reg_a(ir) as usize],
+                _ => break, // HLT or unknown
+            }
+        }
+    }
+    unsafe {
+        println!("ram[64]: {}", RAM[64]);
+    }
+}
+
 pub fn assembler() {
     unsafe {
         ROM[0] = ldh(REG0, 0);
@@ -41,7 +102,7 @@ pub fn assembler() {
         ROM[9] = add(REG0, REG2);
         ROM[10] = st(REG0, 64);
         ROM[11] = cmp(REG2, REG3);
-        ROM[12] = je(14); 
+        ROM[12] = je(14);
         ROM[13] = jmp(8);
         ROM[14] = hlt();
     }
